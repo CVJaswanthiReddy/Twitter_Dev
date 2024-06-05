415d585568a8025b846593c6bfebeaa8b3a570c3
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-06-05 11:01:17 +0530

    Added codes

---
+++ src/index.js
@@ -0,0 +1,58 @@
+const express= require('express');
+const connect= require('./config/database');
+const app=express();
+// const Tweet= require('./models/tweet');
+
+const TweetRepository= require('./repository/tweet-repository');
+const Comment= require('./models/comment');
+app.listen(3000,async () =>{
+    console.log(`server started `);
+    await connect();
+    console.log('Mongo db connected');
+    //for creating tweets
+    // const tweet= await Tweet.create({
+    //     content:'third Tweet',
+       
+    // });
+
+    //fetch the tweets
+    //const tweets= await Tweet.find();
+
+    //fetch by particular id
+    // const tweet= await Tweet.findById('665fe590cb2acd8c3eef22b1');
+    // tweet.userEmail='b@c.com';
+    // await tweet.save();
+    // console.log(tweet);
+
+
+    //to update the tweet
+    // const tweetRepo= new TweetRepository();
+    // const tweet= await tweetRepo.update('665fe59f37e9ce06f3c47eb1',
+    //     {content:'my tweet is working'});
+
+    // const tweetRepo= new TweetRepository();
+    // const tweet= await tweetRepo.create({
+    //     content:'tweet with a comment'
+    // });
+    // tweet.comments.push({
+    //     content:'first comment'
+    // })
+    // await tweet.save();
+    // console.log(tweet);
+
+    //created commentschema model
+
+    // const tweetRepo= new TweetRepository();
+    // const tweet= await tweetRepo.create({
+    //     content:'Tweeet with comment schema'
+    // });
+    // console.log(tweet);
+    // const comment= await Comment.create({content:'new comment'});
+    // tweet.comments.push(comment);
+    // await tweet.save();
+    // console.log(tweet);
+
+    const tweetRepo= new TweetRepository();
+    const tweet= await tweetRepo.getWithComments('665ff1418f524c4fa7da6376');
+    console.log(tweet);
+})
