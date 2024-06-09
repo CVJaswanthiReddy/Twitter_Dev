749a0237c2f0ed4472220c336548c75c499b1c0e
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-06-09 18:40:58 +0530

    moved to es modules

--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -1,4 +1,4 @@
-const { TweetRepository, HashtagRepository }=require('../repository/index')
+import{ TweetRepository, HashtagRepository } from '../repository/index.js'
 
 class TweetService{
     constructor(){
@@ -36,7 +36,7 @@ class TweetService{
     }
 }
 
-module.exports=TweetService;
+export default TweetService;
 
 /**
  * this is my #first #tweet. i am really #excited
