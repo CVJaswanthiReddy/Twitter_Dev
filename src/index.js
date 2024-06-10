7e1fb6337433d1a01d071d4e0d9f669a8e4cd2d9
CVJaswanthiReddy <jaswanthireddy95@gmail.com>
2024-06-10 10:51:37 +0530

    added controllers and routes for create tweet api

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
 import express from 'express';
+
+import bodyParser from 'body-parser';
 import {connect} from './config/database.js'
-import HashtagRepository from './repository/hashtag-repository.js';
+
+import apiRoutes from './routes/index.js'
 const app=express();
 
-import service from './services/tweet-service.js'
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+app.use('/api', apiRoutes);
+
 app.listen(3000,async () =>{
     console.log(`server started `);
     await connect();
     console.log('Mongo db connected');
-    let ser= new service();
-    await ser.create({content:'my other #CoDE #works or #NOT?'})
+    // let ser= new service();
+    // await ser.create({content:'my other #CoDE #works or #NOT?'})
 });
--- src/services/tweet-service.js
+++ src/services/tweet-service.js
@@ -12,19 +12,20 @@ class TweetService{
                         .map((tag) =>tag.substring(1))
                         .map(tag => tag.toLowerCase()); //this regex extracts hashatag
 
-        console.log(tags);
-        const tweet= await this. tweetRepository.create(data)
+        
+        const tweet= await this.tweetRepository.create(data);
         let alreadyPresentTags= await this.hashtagRepository.findByName(tags);
         let titleOfPresentTags=alreadyPresentTags.map(tags => tags.title);
         let newTags= tags.filter(tag => !titleOfPresentTags.includes(tag));
         newTags= newTags.map(tag=>{
-            return {title:tag, tweets:[tweet.id ]}
+            return {title:tag, tweets:[tweet.id]}
         })
         await this.hashtagRepository.bulkCreate(newTags)
         alreadyPresentTags.forEach((tag) => {
             tag.tweets.push(tweet.id);
             tag.save();
         })
+        return tweet;
         //[{title:'coding', tweets:[]}]
         //[excited, coding,js,career]->[{title:excited},title{career}]
         //todocreate hashtags and add here
@@ -33,7 +34,7 @@ class TweetService{
          * 2. filter title of hashtag based on multiple tags
          * 3. How to add tweet id inside all the hashtags
          */
-        return tweet;
+  
         
     }
 }
